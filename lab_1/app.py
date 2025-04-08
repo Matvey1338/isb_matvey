ec175b6e5a92ce69e9e6ead8bd5da006f935d2a9
Matvey1338 <duo163@yandex.ru>
2025-04-08 04:33:37 +0400

    make normal app

--- lab_1/app.py
+++
@@ -1,8 +0,0 @@
-import os
-from flask import Flask, render_template, request, jsonify, session, url_for, redirect, flash
-
-app = Flask(__name__)
-
-
-if __name__ == '__main__':
-    app.run(debug = True)
