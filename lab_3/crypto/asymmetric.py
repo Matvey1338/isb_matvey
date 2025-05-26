9aa7eb37ebec9d62f1723b50bb1b6de30f9d5e7f
Matvey1338 <duo163@yandex.ru>
2025-05-26 15:19:10 +0400

    assymetric update

--- lab_3/crypto/asymmetric.py
+++ lab_3/crypto/asymmetric.py
@@ -1,13 +1,7 @@
-from cryptography.hazmat.primitives.asymmetric import rsa, padding as asym_padding
+from cryptography.hazmat.primitives.asymmetric import padding as asym_padding
 from cryptography.hazmat.primitives import hashes
 
 class AsymmetricCipher:
-    @staticmethod
-    def generate_keys():
-        private = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-        public = private.public_key()
-        return private, public
-
     @staticmethod
     def encrypt(public_key, data: bytes) -> bytes:
         return public_key.encrypt(
