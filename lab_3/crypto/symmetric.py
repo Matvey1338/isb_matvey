a6bf080bb0c4249f5954f4a236c146f6f1c2b978
Matvey1338 <duo163@yandex.ru>
2025-05-26 16:11:44 +0400

    change cipher to IDEA

--- lab_3/crypto/symmetric.py
+++ lab_3/crypto/symmetric.py
@@ -1,23 +1,34 @@
 import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 
 class SymmetricCipher:
     def __init__(self, key: bytes, iv: bytes = None):
+        if len(key) != 16:
+            raise ValueError("IDEA requires a 16-byte key")
         self.key = key
-        self.iv = iv or os.urandom(16)
-        self.cipher = Cipher(algorithms.AES(self.key), modes.CBC(self.iv))
+        self.iv = iv or os.urandom(8)
+        self.cipher = Cipher(
+            algorithms.IDEA(self.key),
+            modes.CBC(self.iv),
+            backend=default_backend()
+        )
 
     def encrypt(self, data: bytes) -> bytes:
-        padder = padding.ANSIX923(128).padder()
-        padded = padder.update(data) + padder.finalize()
+        padder = padding.PKCS7(64).padder()
+        padded_data = padder.update(data) + padder.finalize()
         encryptor = self.cipher.encryptor()
-        return self.iv + encryptor.update(padded) + encryptor.finalize()
+        return self.iv + encryptor.update(padded_data) + encryptor.finalize()
 
     def decrypt(self, token: bytes) -> bytes:
-        iv, ct = token[:16], token[16:]
-        cipher = Cipher(algorithms.AES(self.key), modes.CBC(iv))
+        iv, ct = token[:8], token[8:]
+        cipher = Cipher(
+            algorithms.IDEA(self.key),
+            modes.CBC(iv),
+            backend=default_backend()
+        )
         decryptor = cipher.decryptor()
         padded = decryptor.update(ct) + decryptor.finalize()
-        unpadder = padding.ANSIX923(128).unpadder()
+        unpadder = padding.PKCS7(64).unpadder()
         return unpadder.update(padded) + unpadder.finalize()
